949aa29e1c216d46919b7aaccce9d930738b1dc6
Gg <58531623+djghis@users.noreply.github.com>
2020-10-15 12:53:43 +0100

    treyng to submit contact form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -3,13 +3,18 @@ import React from 'react'
 
 const ContactForm = () => {
     return (
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="text" name="name" placeholder="Your message here...">
-              
-        </input>
-        <button>
+      <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field">
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact"/>
+        <label>
+        Message: 
+            <input type="text" name="message" placeholder="Your message here...">    
+            </input>
+        </label>
+        <button type="submit">
                Send   
         </button>
+        <input type="reset" value="clear" />
       </form>   
     )
 }
