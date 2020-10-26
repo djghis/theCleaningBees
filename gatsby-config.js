99b320583b8f88e7025a92236b5796d61313fd51
Gg <58531623+djghis@users.noreply.github.com>
2020-10-26 14:42:53 +0000

    change color theme in helmet for mobile phone browsing

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#ff0`,
-        theme_color: `#663399`,
+        theme_color: ` #547E6C`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
